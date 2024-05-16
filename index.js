b991c2837beb8432e11071275888491cae478b82
jincheng <jcdby@hotmail.com>
2024-05-16 23:03:04 +0800

    fix issue of verification

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.post("/check", async (req, res) => {
       if (MsgType === "event") {
         Event = receiveMsg.xml.Event[0];
         EventKey = receiveMsg.xml.EventKey[0];
-        if (Event === "SCAN") {
+        if (Event === "SCAN" || Event === "subscribe") {
           try {
             const code = await genOtp(EventKey, FromUserName);
 
